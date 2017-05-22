ece311eb5243c1e48537282e2bb4c7f63f462384
佐藤正康 <satohmsys@gmail.com>
2017-05-23 02:34:40 +0900

    about | レスポンシブ対応

--- portfolio/about/js/script.js
+++ portfolio/about/js/script.js
@@ -24,18 +24,10 @@ spaning( document.getElementsByClassName('pagevisual_copy_pagename')[0] );
 //         }) )
 //     .addTo( controller );
 // })();
-
-		$scene =  new ScrollMagic.Scene({
-			triggerElement: document.getElementsByClassName('pagevisual')[0],
-			triggetHook: 'onEnter'
-		})
-		// trigger animation by adding a css class
-		.setClassToggle( document.getElementsByClassName('pagevisual')[0], "inview" )
-        .on("enter", function (event) {  // シーンの状態が"DURING"に入る際に発火する
-        })
-        .on("leave", function (event) { // シーンの状態が"DURING"から"BEFORE"か"AFTER"に移る際に発火する
-        })
-        .on("progress", function (event) { // シーン変化の度に呼ばれる
-        })
-        // .addIndicators({name: "1 - add a class"}) // add indicators (requires plugin)
-		.addTo(controller);
+    
+$scene =  new ScrollMagic.Scene({
+	triggerElement: document.getElementsByClassName('pagevisual')[0],
+	triggetHook: 'onEnter'
+})
+.setClassToggle( document.getElementsByClassName('pagevisual')[0], "inview" )
+.addTo(controller);

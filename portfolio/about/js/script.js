ee2dd9fede7ea3636aa41beb6949a82dfa693281
佐藤正康 <satohmsys@gmail.com>
2017-06-02 11:11:19 +0900

    about bio | レイアウト、レスポンシブ対応

--- portfolio/about/js/script.js
+++ portfolio/about/js/script.js
@@ -1,5 +1,9 @@
-var $sections = document.getElementsByClassName( 'section' );    
-    controller = new ScrollMagic.Controller();
+var $timelineEvents = document.getElementsByClassName( 'timeline_event' );    
+    controller = new ScrollMagic.Controller(),
+    $line = document.getElementsByClassName('timeline_line')[0],
+    $heightOfLine = $line.clientHeight;
+
+    console.log( $heightOfLine )
 /**
 * SPANで囲む
 */
@@ -7,22 +11,24 @@ spaning( document.getElementsByClassName('pagevisual_copy_pagename')[0] );
 
 
 
-
 /**
 * scrollMagic 
 */
-// first view   
+
+// first view  
 new ScrollMagic.Scene({
-	triggerElement: document.getElementsByClassName('pagevisual')[0],
-	triggetHook: 'onEnter'
+    triggerElement: document.getElementsByClassName('pagevisual')[0],
+    triggetHook: 'onEnter'
 })
 .setClassToggle( document.getElementsByClassName('pagevisual')[0], "inview" )
-.addTo(controller);
+.addTo(controller); 
+
 
 //section
-Array.prototype.forEach.call( $sections, function( e ){
+Array.prototype.forEach.call( $timelineEvents, function( e ){
+
     var $section =  e ,
-        $sectionClassName = '.' + $section.className.match( 'section-.*' )[0],
+        $sectionClassName = '.' + $section.className.match( 'tl[0-9].*' )[0],
         $scene =  new ScrollMagic.Scene({
             triggerElement: $sectionClassName,
             triggetHook: 'onEnter'
@@ -31,6 +37,10 @@ Array.prototype.forEach.call( $sections, function( e ){
         .setClassToggle( $sectionClassName, "inview" )
         .on("enter", function (event) {  // シーンの状態が"DURING"に入る際に発火する
             console.log("Scene entered.",event);
+            // console.log( this );
+            // var barheight = document.getElementsByClassName( $section.className.match( 'tl[0-9].*' ) )[0].offsetHeight;
+            // $line.setAttribute('style', 'height:' + $heightOfLine + barheight + 'px');
+            // console.log( document.getElementsByClassName( $section.className.match( 'tl[0-9].*' ) )[0].style )
         })
         .on("leave", function (event) { // シーンの状態が"DURING"から"BEFORE"か"AFTER"に移る際に発火する
             this.setClassToggle( $sectionClassName, "outview" )
